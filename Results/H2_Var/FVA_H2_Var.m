13e653efdb61aa58222f88f1bbdcfd554a7c3834
ParsaGhadermazi <54489047+ParsaGhadermazi@users.noreply.github.com>
2021-12-11 19:40:20 -0700

    FVA analysis

---
+++ Results/H2_Var/FVA_H2_Var.m
@@ -0,0 +1,30 @@
+Metaclau=readCbModel('Metaclau.mat')
+load('EXP_Struct.mat')
+
+for i=1:size(EXP_Struct,2)
+    Uptakes.rxns{i}=EXP_Struct(i).Name;
+    Uptakes.values{i}=EXP_Struct(i).EXP1;                                     %If you want another experiment, add that to EXP_Struct, and change EXP1 accordingly in this line
+    Uptakes.type{i}='b';
+end
+Metaclau=changeRxnBounds(Metaclau,Metaclau.rxns(789),Uptakes.values{1});
+Metaclau.rxns(find(Metaclau.c))
+inds=[788,771,772,790,786,787,849]
+i=1;
+for Hyd_Ups=0:-0.5:-100
+    Temp_Meta=changeRxnBounds(Metaclau,'EX_HYDROGEN-MOLECULE',Hyd_Ups,'l')
+    [min,max]=fluxVariability(Temp_Meta, 'optPercentage', 75, 'allowLoops',1,'printLevel',2,'rxnNameList',Temp_Meta.rxns(inds),'method','1-norm');
+    Min_Sol(:,i)=min;
+    Max_Sol(:,i)=max;
+    i=i+1;
+
+end
+
+for J=1:size(Min_Sol,1)
+    subplot(size(Min_Sol,1),1,J)
+    plot(-0:-0.5:-100,Min_Sol(J,:),'.',0:-0.5:-100,Max_Sol(J,:),'.')
+    legend({'Min','Max'})
+    title(regexprep(Metaclau.rxns(inds(J)),'_','-'))
+    xlabel('Hydrogen Uptake Flux')
+    ylabel('Exchange Flux')
+end
+
